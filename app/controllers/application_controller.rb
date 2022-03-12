6db168dbfbc53eecea49438fa5c7245baacf370c
EC2 Default User <ec2-user@ip-172-31-16-98.ec2.internal>
2022-03-12 08:01:33 +0000

    [Finish]2-25

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,22 @@
 class ApplicationController < ActionController::Base
+  before_action :authenticate_user!, except: [:top, :about]
+
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def after_sign_in_path_for(resource)
+    flash[:notice] = "User was successfully login."
+    user_path(resource)
+  end
+
+  def after_sign_out_path_for(resource)
+     flash[:notice] = "User was successfully logout."
+    root_path
+  end
+
+  private
+
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.permit(:sign_up,keys:[:email]) # 注目
+    end
 end
+
